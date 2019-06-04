20c77713f376bb4c6a07b9e59bd63d084f36ec3f
Andre <andre@andrevlopes.com>
2019-06-03 23:00:09 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
 require_once("config.php");
+//carrega um usuario
+//$root = new Usuario();
+//$root-> loadByid(2);
 
-$root = new Usuario();
-$root-> loadByid(2);
+//echo $root;
 
-echo $root;
+// carrega uma lista usuario
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+// carrega uma lista do login
+//$search = Usuario::search("an");
+//echo json_encode($search);
+
+//carrega usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("Andre lopes", "v123jkl");
+
+echo $usuario;
+
+
 ?>
